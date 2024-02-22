977e456b0e17b10d82b23a0b3431ecf1f977f83e
LukaChazarretaCoronel <78001855+LukaChazarretaCoronel@users.noreply.github.com>
2024-02-22 13:04:13 -0300

    Fill the Python file

---
+++ main.py
@@ -0,0 +1,6 @@
+def add_expense(expenses, amount, category):
+    expenses.append({'amount':amount,'category': category})
+expenses = []
+def print_expenses(expenses):
+    for expense in expenses:
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
